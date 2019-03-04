3648194b29d3a2a0d1caad508206e93f695c270c
Paige Herbst <PMoney@Paiges-MacBook-Pro.local>
2019-03-04 14:48:08 -0700

    dfsdafd

---
+++ clicky/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ClickyGame from './components/ClickyGame';
+import './index.css';
+ReactDOM.render(<ClickyGame />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+
