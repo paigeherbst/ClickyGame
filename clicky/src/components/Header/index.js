37b5462218a4fb88e0316588e01f1c5887376a94
Paige Herbst <PMoney@Paiges-MacBook-Pro.local>
2019-03-20 13:58:15 -0600

    changes

---
+++ clicky/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Clicky Game!</h1>
+      <h2>Everytime you click on an image you earn points! Only click one button at a time though!</h2>
+    </header>
+  );
+}
+
+export default Header;
--- clicky/src/index.js
+++ clicky/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ClickyGame from './components/ClickyGame';
+import App from "./App";
 import './index.css';
-ReactDOM.render(<ClickyGame />, document.getElementById('root'));
+import registerServiceWorker from "./registerServiceWorker";
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
